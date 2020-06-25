b2f4fc5a16d75f4058f69cf295614fe1dd36baee
lyes <lyes.makhloufi@outlook.fr>
2020-06-25 14:49:55 +0200

    insertion sort

--- src/Test.java
+++ src/Test.java
@@ -20,6 +20,23 @@ public class Test {
 
 	}
 	
+	 /* this kind of sorting is powerful than others when the array is almost sorted, in the opposite when the 
+	  * array is sorted upside down
+	  * */
+	public static void insertionSort(Double[] doubles) {
+		int pos = 0;
+		double temp;
+		for(int i = 0; i < doubles.length;i++) {
+			for(int j = 0;j<pos;j++) {
+				if (doubles[j] > doubles[i]) {
+					temp = doubles[j];
+					doubles[j] = doubles[i];
+					doubles[i] = temp;
+				}
+			}
+			pos++;
+		}
+	}
 	
 	
 	
@@ -33,10 +50,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		Double[] doubles = new Double[] {1.00,2.00,4.55,18.36,16.25,10.25,17.2,3.33,5.55};
+		System.out.println("Initial array");
 		printArray(doubles);
-		bubbleSort(doubles);
 		System.out.println("***************************");
+		bubbleSort(doubles);
 		printArray(doubles);
+		System.out.println("***************************");
+		Double[] doubles1 = new Double[] {5.12,1.22,48.22,6.01,14.25,25.35,0.2123,0.012};
+		printArray(doubles1);
+		System.out.println("***************************");
+		insertionSort(doubles1);
+		printArray(doubles1);
+		System.out.println("***************************");
+		
 
 	}
 
